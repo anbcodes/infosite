f6eea0c724e49e8db822076310b38c92944cb882
anbcodes <anbcodes@pm.me>
2021-09-30 17:59:14 +0000

    Deploying to gh-pages from @ anbcodes/infosite@80e5d45afb2f04fa7be6b895fd4579970d17625c 🚀

---
+++ _app/pages/disclaimer.svelte-a2e4cf22.js
@@ -0,0 +1,33 @@
+import{S as le,i as ce,s as he,e as i,t as n,k as h,c as r,a as l,g as a,d as s,n as d,b as Q,f as de,E as e,G as R}from"../chunks/vendor-4cd5d097.js";function ue(re){let t,y,D,S,p,q,x,b,A,G,c,H,u,T,C,m,I,B,F,w,O,U,v,W,j,g,V,z,k,J,K,_,L,M,P,N;return{c(){t=i("div"),y=i("h1"),D=n("Disclaimer for anbcodes"),S=h(),p=i("p"),q=n(`If you require any more information or have any questions about our site's disclaimer, please
+		feel free to contact us by email at anbcodes@protonmail.com`),x=h(),b=i("h2"),A=n("Disclaimers for anbcodes"),G=h(),c=i("p"),H=n(`All the information on this website - anb.codes - is published in good faith and for general
+		information purpose only. anbcodes does not make any warranties about the completeness,
+		reliability and accuracy of this information. Any action you take upon the information you find
+		on this website (anbcodes), is strictly at your own risk. anbcodes will not be liable for any
+		losses and/or damages in connection with the use of our website. Our Disclaimer was generated
+		with the help of the `),u=i("a"),T=n("Disclaimer Generator"),C=n(`
+		and the `),m=i("a"),I=n("Privacy Policy Generator"),B=n("."),F=h(),w=i("p"),O=n(`From our website, you can visit other websites by following hyperlinks to such external sites.
+		While we strive to provide only quality links to useful and ethical websites, we have no control
+		over the content and nature of these sites. These links to other websites do not imply a
+		recommendation for all the content found on these sites. Site owners and content may change
+		without notice and may occur before we have the opportunity to remove a link which may have gone
+		'bad'.`),U=h(),v=i("p"),W=n(`Please be also aware that when you leave our website, other sites may have different privacy
+		policies and terms which are beyond our control. Please be sure to check the Privacy Policies of
+		these sites as well as their "Terms of Service" before engaging in any business or uploading any
+		information.`),j=h(),g=i("h2"),V=n("Consent"),z=h(),k=i("p"),J=n("By using our website, you hereby consent to our disclaimer and agree to its terms."),K=h(),_=i("h2"),L=n("Update"),M=h(),P=i("p"),N=n(`Should we update, amend or make any changes to this document, those changes will be prominently
+		posted here.`),this.h()},l(E){t=r(E,"DIV",{class:!0});var o=l(t);y=r(o,"H1",{});var X=l(y);D=a(X,"Disclaimer for anbcodes"),X.forEach(s),S=d(o),p=r(o,"P",{});var Y=l(p);q=a(Y,`If you require any more information or have any questions about our site's disclaimer, please
+		feel free to contact us by email at anbcodes@protonmail.com`),Y.forEach(s),x=d(o),b=r(o,"H2",{});var Z=l(b);A=a(Z,"Disclaimers for anbcodes"),Z.forEach(s),G=d(o),c=r(o,"P",{});var f=l(c);H=a(f,`All the information on this website - anb.codes - is published in good faith and for general
+		information purpose only. anbcodes does not make any warranties about the completeness,
+		reliability and accuracy of this information. Any action you take upon the information you find
+		on this website (anbcodes), is strictly at your own risk. anbcodes will not be liable for any
+		losses and/or damages in connection with the use of our website. Our Disclaimer was generated
+		with the help of the `),u=r(f,"A",{href:!0});var $=l(u);T=a($,"Disclaimer Generator"),$.forEach(s),C=a(f,`
+		and the `),m=r(f,"A",{href:!0});var ee=l(m);I=a(ee,"Privacy Policy Generator"),ee.forEach(s),B=a(f,"."),f.forEach(s),F=d(o),w=r(o,"P",{});var te=l(w);O=a(te,`From our website, you can visit other websites by following hyperlinks to such external sites.
+		While we strive to provide only quality links to useful and ethical websites, we have no control
+		over the content and nature of these sites. These links to other websites do not imply a
+		recommendation for all the content found on these sites. Site owners and content may change
+		without notice and may occur before we have the opportunity to remove a link which may have gone
+		'bad'.`),te.forEach(s),U=d(o),v=r(o,"P",{});var oe=l(v);W=a(oe,`Please be also aware that when you leave our website, other sites may have different privacy
+		policies and terms which are beyond our control. Please be sure to check the Privacy Policies of
+		these sites as well as their "Terms of Service" before engaging in any business or uploading any
+		information.`),oe.forEach(s),j=d(o),g=r(o,"H2",{});var ne=l(g);V=a(ne,"Consent"),ne.forEach(s),z=d(o),k=r(o,"P",{});var ae=l(k);J=a(ae,"By using our website, you hereby consent to our disclaimer and agree to its terms."),ae.forEach(s),K=d(o),_=r(o,"H2",{});var se=l(_);L=a(se,"Update"),se.forEach(s),M=d(o),P=r(o,"P",{});var ie=l(P);N=a(ie,`Should we update, amend or make any changes to this document, those changes will be prominently
+		posted here.`),ie.forEach(s),o.forEach(s),this.h()},h(){Q(u,"href","https://www.privacypolicyonline.com/disclaimer-generator/"),Q(m,"href","https://www.generateprivacypolicy.com"),Q(t,"class","")},m(E,o){de(E,t,o),e(t,y),e(y,D),e(t,S),e(t,p),e(p,q),e(t,x),e(t,b),e(b,A),e(t,G),e(t,c),e(c,H),e(c,u),e(u,T),e(c,C),e(c,m),e(m,I),e(c,B),e(t,F),e(t,w),e(w,O),e(t,U),e(t,v),e(v,W),e(t,j),e(t,g),e(g,V),e(t,z),e(t,k),e(k,J),e(t,K),e(t,_),e(_,L),e(t,M),e(t,P),e(P,N)},p:R,i:R,o:R,d(E){E&&s(t)}}}class fe extends le{constructor(t){super();ce(this,t,null,ue,he,{})}}export{fe as default};
---
+++ _app/pages/privacy.svelte-a56aff0c.js
@@ -0,0 +1,73 @@
+import{S as mt,i as bt,s as gt,e as s,t as i,k as l,c as a,a as c,g as n,d as r,n as h,b as F,f as wt,E as e,G as Ne}from"../chunks/vendor-4cd5d097.js";function Pt(vt){let t,g,J,N,w,U,j,P,V,K,k,M,Q,d,X,f,Z,$,u,ee,te,oe,T,ie,ne,_,re,se,p,ae,y,ce,le,he,E,de,pe,v,fe,m,ue,ye,I,ve,me,C,be,ge,A,we,Pe,G,ke,Te,H,_e,Ee,W,Ie,Ce,x,Ae,Ge,D,He,We,z,xe,De,R,ze,Re,S,Se,Be,B,Le,Oe,L,qe,Fe,O,Ye;return{c(){t=s("div"),g=s("h1"),J=i("Privacy Policy for anbcodes"),N=l(),w=s("p"),U=i(`At anbcodes, accessible from anb.codes, one of our main priorities is the privacy of our
+		visitors. This Privacy Policy document contains types of information that is collected and
+		recorded by anbcodes and how we use it.`),j=l(),P=s("p"),V=i(`If you have additional questions or require more information about our Privacy Policy, do not
+		hesitate to contact us.`),K=l(),k=s("h2"),M=i("Log Files"),Q=l(),d=s("p"),X=i(`anbcodes follows a standard procedure of using log files. These files log visitors when they
+		visit websites. All hosting companies do this and a part of hosting services' analytics. The
+		information collected by log files include internet protocol (IP) addresses, browser type,
+		Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the
+		number of clicks. These are not linked to any information that is personally identifiable. The
+		purpose of the information is for analyzing trends, administering the site, tracking users'
+		movement on the website, and gathering demographic information. Our Privacy Policy was created
+		with the help of the `),f=s("a"),Z=i("Privacy Policy Generator"),$=i(`
+		and the `),u=s("a"),ee=i("Privacy Policy Generator"),te=i("."),oe=l(),T=s("h2"),ie=i("Cookies and Web Beacons"),ne=l(),_=s("p"),re=i(`Like any other website, anbcodes uses 'cookies'. These cookies are used to store information
+		including visitors' preferences, and the pages on the website that the visitor accessed or
+		visited. The information is used to optimize the users' experience by customizing our web page
+		content based on visitors' browser type and/or other information.`),se=l(),p=s("p"),ae=i('For more general information on cookies, please read the "What Are Cookies" article on '),y=s("a"),ce=i("Cookie Consent website"),le=i("."),he=l(),E=s("h2"),de=i("Google DoubleClick DART Cookie"),pe=l(),v=s("p"),fe=i(`Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies,
+		to serve ads to our site visitors based upon their visit to anb.codes and other sites on the
+		internet. However, visitors may choose to decline the use of DART cookies by visiting the Google
+		ad and content network Privacy Policy at the following URL \u2013 `),m=s("a"),ue=i("https://policies.google.com/technologies/ads"),ye=l(),I=s("h2"),ve=i("Privacy Policies"),me=l(),C=s("p"),be=i(`You may consult this list to find the Privacy Policy for each of the advertising partners of
+		anbcodes.`),ge=l(),A=s("p"),we=i(`Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons
+		that are used in their respective advertisements and links that appear on anbcodes, which are
+		sent directly to users' browser. They automatically receive your IP address when this occurs.
+		These technologies are used to measure the effectiveness of their advertising campaigns and/or
+		to personalize the advertising content that you see on websites that you visit.`),Pe=l(),G=s("p"),ke=i(`Note that anbcodes has no access to or control over these cookies that are used by third-party
+		advertisers.`),Te=l(),H=s("h2"),_e=i("Third Party Privacy Policies"),Ee=l(),W=s("p"),Ie=i(`anbcodes's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising
+		you to consult the respective Privacy Policies of these third-party ad servers for more detailed
+		information. It may include their practices and instructions about how to opt-out of certain
+		options.`),Ce=l(),x=s("p"),Ae=i(`You can choose to disable cookies through your individual browser options. To know more detailed
+		information about cookie management with specific web browsers, it can be found at the browsers'
+		respective websites. What Are Cookies?`),Ge=l(),D=s("h2"),He=i("Children's Information"),We=l(),z=s("p"),xe=i(`Another part of our priority is adding protection for children while using the internet. We
+		encourage parents and guardians to observe, participate in, and/or monitor and guide their
+		online activity.`),De=l(),R=s("p"),ze=i(`anbcodes does not knowingly collect any Personal Identifiable Information from children under
+		the age of 13. If you think that your child provided this kind of information on our website, we
+		strongly encourage you to contact us immediately and we will do our best efforts to promptly
+		remove such information from our records.`),Re=l(),S=s("h2"),Se=i("Online Privacy Policy Only"),Be=l(),B=s("p"),Le=i(`This Privacy Policy applies only to our online activities and is valid for visitors to our
+		website with regards to the information that they shared and/or collect in anbcodes. This policy
+		is not applicable to any information collected offline or via channels other than this website.`),Oe=l(),L=s("h2"),qe=i("Consent"),Fe=l(),O=s("p"),Ye=i(`By using our website, you hereby consent to our Privacy Policy and agree to its Terms and
+		Conditions.`),this.h()},l(q){t=a(q,"DIV",{class:!0});var o=c(t);g=a(o,"H1",{});var Ue=c(g);J=n(Ue,"Privacy Policy for anbcodes"),Ue.forEach(r),N=h(o),w=a(o,"P",{});var je=c(w);U=n(je,`At anbcodes, accessible from anb.codes, one of our main priorities is the privacy of our
+		visitors. This Privacy Policy document contains types of information that is collected and
+		recorded by anbcodes and how we use it.`),je.forEach(r),j=h(o),P=a(o,"P",{});var Ve=c(P);V=n(Ve,`If you have additional questions or require more information about our Privacy Policy, do not
+		hesitate to contact us.`),Ve.forEach(r),K=h(o),k=a(o,"H2",{});var Ke=c(k);M=n(Ke,"Log Files"),Ke.forEach(r),Q=h(o),d=a(o,"P",{});var b=c(d);X=n(b,`anbcodes follows a standard procedure of using log files. These files log visitors when they
+		visit websites. All hosting companies do this and a part of hosting services' analytics. The
+		information collected by log files include internet protocol (IP) addresses, browser type,
+		Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the
+		number of clicks. These are not linked to any information that is personally identifiable. The
+		purpose of the information is for analyzing trends, administering the site, tracking users'
+		movement on the website, and gathering demographic information. Our Privacy Policy was created
+		with the help of the `),f=a(b,"A",{href:!0});var Me=c(f);Z=n(Me,"Privacy Policy Generator"),Me.forEach(r),$=n(b,`
+		and the `),u=a(b,"A",{href:!0});var Qe=c(u);ee=n(Qe,"Privacy Policy Generator"),Qe.forEach(r),te=n(b,"."),b.forEach(r),oe=h(o),T=a(o,"H2",{});var Xe=c(T);ie=n(Xe,"Cookies and Web Beacons"),Xe.forEach(r),ne=h(o),_=a(o,"P",{});var Ze=c(_);re=n(Ze,`Like any other website, anbcodes uses 'cookies'. These cookies are used to store information
+		including visitors' preferences, and the pages on the website that the visitor accessed or
+		visited. The information is used to optimize the users' experience by customizing our web page
+		content based on visitors' browser type and/or other information.`),Ze.forEach(r),se=h(o),p=a(o,"P",{});var Y=c(p);ae=n(Y,'For more general information on cookies, please read the "What Are Cookies" article on '),y=a(Y,"A",{href:!0});var $e=c(y);ce=n($e,"Cookie Consent website"),$e.forEach(r),le=n(Y,"."),Y.forEach(r),he=h(o),E=a(o,"H2",{});var et=c(E);de=n(et,"Google DoubleClick DART Cookie"),et.forEach(r),pe=h(o),v=a(o,"P",{});var Je=c(v);fe=n(Je,`Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies,
+		to serve ads to our site visitors based upon their visit to anb.codes and other sites on the
+		internet. However, visitors may choose to decline the use of DART cookies by visiting the Google
+		ad and content network Privacy Policy at the following URL \u2013 `),m=a(Je,"A",{href:!0});var tt=c(m);ue=n(tt,"https://policies.google.com/technologies/ads"),tt.forEach(r),Je.forEach(r),ye=h(o),I=a(o,"H2",{});var ot=c(I);ve=n(ot,"Privacy Policies"),ot.forEach(r),me=h(o),C=a(o,"P",{});var it=c(C);be=n(it,`You may consult this list to find the Privacy Policy for each of the advertising partners of
+		anbcodes.`),it.forEach(r),ge=h(o),A=a(o,"P",{});var nt=c(A);we=n(nt,`Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons
+		that are used in their respective advertisements and links that appear on anbcodes, which are
+		sent directly to users' browser. They automatically receive your IP address when this occurs.
+		These technologies are used to measure the effectiveness of their advertising campaigns and/or
+		to personalize the advertising content that you see on websites that you visit.`),nt.forEach(r),Pe=h(o),G=a(o,"P",{});var rt=c(G);ke=n(rt,`Note that anbcodes has no access to or control over these cookies that are used by third-party
+		advertisers.`),rt.forEach(r),Te=h(o),H=a(o,"H2",{});var st=c(H);_e=n(st,"Third Party Privacy Policies"),st.forEach(r),Ee=h(o),W=a(o,"P",{});var at=c(W);Ie=n(at,`anbcodes's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising
+		you to consult the respective Privacy Policies of these third-party ad servers for more detailed
+		information. It may include their practices and instructions about how to opt-out of certain
+		options.`),at.forEach(r),Ce=h(o),x=a(o,"P",{});var ct=c(x);Ae=n(ct,`You can choose to disable cookies through your individual browser options. To know more detailed
+		information about cookie management with specific web browsers, it can be found at the browsers'
+		respective websites. What Are Cookies?`),ct.forEach(r),Ge=h(o),D=a(o,"H2",{});var lt=c(D);He=n(lt,"Children's Information"),lt.forEach(r),We=h(o),z=a(o,"P",{});var ht=c(z);xe=n(ht,`Another part of our priority is adding protection for children while using the internet. We
+		encourage parents and guardians to observe, participate in, and/or monitor and guide their
+		online activity.`),ht.forEach(r),De=h(o),R=a(o,"P",{});var dt=c(R);ze=n(dt,`anbcodes does not knowingly collect any Personal Identifiable Information from children under
+		the age of 13. If you think that your child provided this kind of information on our website, we
+		strongly encourage you to contact us immediately and we will do our best efforts to promptly
+		remove such information from our records.`),dt.forEach(r),Re=h(o),S=a(o,"H2",{});var pt=c(S);Se=n(pt,"Online Privacy Policy Only"),pt.forEach(r),Be=h(o),B=a(o,"P",{});var ft=c(B);Le=n(ft,`This Privacy Policy applies only to our online activities and is valid for visitors to our
+		website with regards to the information that they shared and/or collect in anbcodes. This policy
+		is not applicable to any information collected offline or via channels other than this website.`),ft.forEach(r),Oe=h(o),L=a(o,"H2",{});var ut=c(L);qe=n(ut,"Consent"),ut.forEach(r),Fe=h(o),O=a(o,"P",{});var yt=c(O);Ye=n(yt,`By using our website, you hereby consent to our Privacy Policy and agree to its Terms and
+		Conditions.`),yt.forEach(r),o.forEach(r),this.h()},h(){F(f,"href","https://www.privacypolicyonline.com/privacy-policy-generator/"),F(u,"href","https://www.generateprivacypolicy.com"),F(y,"href","https://www.cookieconsent.com/what-are-cookies/"),F(m,"href","https://policies.google.com/technologies/ads"),F(t,"class","")},m(q,o){wt(q,t,o),e(t,g),e(g,J),e(t,N),e(t,w),e(w,U),e(t,j),e(t,P),e(P,V),e(t,K),e(t,k),e(k,M),e(t,Q),e(t,d),e(d,X),e(d,f),e(f,Z),e(d,$),e(d,u),e(u,ee),e(d,te),e(t,oe),e(t,T),e(T,ie),e(t,ne),e(t,_),e(_,re),e(t,se),e(t,p),e(p,ae),e(p,y),e(y,ce),e(p,le),e(t,he),e(t,E),e(E,de),e(t,pe),e(t,v),e(v,fe),e(v,m),e(m,ue),e(t,ye),e(t,I),e(I,ve),e(t,me),e(t,C),e(C,be),e(t,ge),e(t,A),e(A,we),e(t,Pe),e(t,G),e(G,ke),e(t,Te),e(t,H),e(H,_e),e(t,Ee),e(t,W),e(W,Ie),e(t,Ce),e(t,x),e(x,Ae),e(t,Ge),e(t,D),e(D,He),e(t,We),e(t,z),e(z,xe),e(t,De),e(t,R),e(R,ze),e(t,Re),e(t,S),e(S,Se),e(t,Be),e(t,B),e(B,Le),e(t,Oe),e(t,L),e(L,qe),e(t,Fe),e(t,O),e(O,Ye)},p:Ne,i:Ne,o:Ne,d(q){q&&r(t)}}}class Tt extends mt{constructor(t){super();bt(this,t,null,Pt,gt,{})}}export{Tt as default};
