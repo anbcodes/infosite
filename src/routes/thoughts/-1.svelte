35a0a8f86ed3b6e9cf16f7fd820d4ee25232d588
anbcodes <anbcodes@protonmail.com>
2021-12-18 00:07:08 -0500

    stages of learning (draft)

---
+++ src/routes/thoughts/-1.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>You found it!</title>
+</svelte:head>
+
+<div class="header-c">You found it!</div>
+
+<p>This is my secret page that serves no reason! Good job finding it!</p>
---
+++ src/routes/thoughts/-2.svelte
@@ -0,0 +1,43 @@
+<svelte:head>
+	<title>Template</title>
+</svelte:head>
+
+<div class="header-c">Template</div>
+
+<p>Template content</p>
+
+<p class="mt-5">Comments</p>
+<p>
+	There are currently no comments. If you have a response or thought about this post (or about another
+	comment), email it to me <a href="mailto:anbcodes@protonmail.com">anbcodes@protonmail.com</a> and I
+	will consider adding it as a comment
+</p>
+
+<p class="pt-5 text-gray-400" xmlns:cc="http://creativecommons.org/ns#">
+	<a class="darklink-nopad" rel="cc:attributionURL" href="https://anb.codes/thoughts/0">This work</a
+	>
+	by
+	<a
+		class="darklink-nopad"
+		rel="cc:attributionURL dct:creator"
+		property="cc:attributionName"
+		href="https://anb.codes">anbcodes</a
+	>
+	is licensed under
+	<a
+		class="darklink-nopad"
+		href="http://creativecommons.org/licenses/by/4.0/?ref=chooser-v1"
+		target="_blank"
+		rel="license noopener noreferrer"
+		style="display:inline-flex;"
+		>CC BY 4.0<img
+			style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+			src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
+			alt="cc by"
+		/><img
+			style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+			src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
+			alt="cc by"
+		/></a
+	>
+</p>
