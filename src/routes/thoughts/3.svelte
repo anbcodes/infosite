b692aebeb1a97d54fe13d075e4376c0418767415
anbcodes <me@anb.codes>
2021-12-30 22:12:01 -0500

    updated articles

---
+++ src/routes/thoughts/3.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Article from '../../components/Article.svelte';
+</script>
+
+<Article title="On Trust">
+	<p>
+		Who should you trust and why? We live in a complex world, full of competing authorities and
+		ideas. Many people, both kids and adults, make mistakes when choosing good authorities (myself
+		included). They don't always check those authorities against evidence. This article will cover
+		who I trust, the difference between peers and authority, and how authority should not be
+		unchecked.
+	</p>
+	<p>
+		I trust many different people, and they have different levels of trust. As a Christian, God and
+		the Bible have unconditional trust, but not without evidence. I respect my parents very much,
+		and they have authority over me, but God has authority over them as he is the ultimate
+		authority. Lastly, even though I don't necessarily trust government officals all the time, they
+		are an authority in my life and deserve respect. Therefore, I respect them.
+	</p>
+	<p>
+		Other people who are commonly trusted are peers. The problem with giving a significant amount of
+		trust to your peers is that most information you get from them is either with barely any
+		evidence or is hearsay. Often stuff that I hear from peers (either my parent's peers or my
+		peers) comes from a chain of one person telling another person something and so on. If you've
+		ever played a game of telephone, you'll know that a chain of people is not a very reliable way
+		to convey information. Context is removed, the ideas are reinterpreted, etc. With social media,
+		this is even worse, mainly because information can get transformed by a chain of people even
+		faster. Lastly, when information comes from a chain of people or posts, it can become impossible
+		to find the original source, which means you can't even discover what was originally said.
+		Because you can't find the sources, the ideas can be transformed, and it ends up being hearsay,
+		peers should not often be trusted unless you go back to the original source.
+	</p>
+	<p>
+		Lastly, all authorities should be fact-checked. Always look for more sources saying the same
+		thing, and if you find a source saying a different thing, don't dismiss it, even if it looks
+		like it comes from a source you don't agree with, instead determine if they have reasons and if
+		the reasons are valid. I believe one way people get sucked into misinformation is by believing
+		that their source is trustworthy while also believing that all other sources are wrong without
+		even checking their reasons. I, as a Christian, even check the Bible against evidence
+		(apologetics), and I don't want to be believing everything I hear blindly. You should check all
+		authorities against other authorities and evidence, and you shouldn't ever dismiss opposing
+		ideas without thought.
+	</p>
+	<p>
+		In conclusion, I carefully choose who I trust, I don't give much trust to peers, and I check
+		authorities against other authorities. You have to trust some people, but you should be careful
+		about who you trust. Keep an open mind and don't get stuck staring at only one side of a debate.
+	</p>
+</Article>
