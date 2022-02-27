653f31d3f57fd7a6e617e84ffdf4ec2e65a93661
anbcodes <anbcodes@pm.me>
2022-02-27 20:17:22 +0000

    Deploying to gh-pages from @ anbcodes/infosite@f0a2dc0a577814e8fcfa2ffcf6c72cfbaaefde68 🚀

--- _app/chunks/thoughts-f42e3811.js
+++
@@ -1,1 +0,0 @@
-const e=[{date:"Decemeber 30th, 2021",title:"On Trust",id:"3"},{date:"Decemeber 27th, 2021",title:"Covid-19 and Faith",id:"2"},{date:"Decemeber 18th, 2021",title:"Observations About Stages of Learning",id:"1"},{date:"Decemeber 4th, 2021",title:"2021: A Year of Renewal And New Beginnings",id:"0"}];export{e as t};
